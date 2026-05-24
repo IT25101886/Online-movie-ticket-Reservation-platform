707b918d3f38cfadd4c286adb9b857af1bf90238
IT25101852 <IT25101852@my.sliit.lk>
2026-05-24 14:26:58 +0530

    Bookings List

---
+++ backend/src/main/java/com/movie/backend/booking/dto/CreateBookingRequest.java
@@ -0,0 +1,88 @@
+package com.movie.backend.booking.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CreateBookingRequest {
+    private Long userId;
+    private Long movieId;
+    private String contactName;
+    private String contactEmail;
+    private String contactMobile;
+    private LocalDate showDate;
+    private String showTime;
+    private String seatType;
+    private List<String> selectedSeats;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactMobile() {
+        return contactMobile;
+    }
+
+    public void setContactMobile(String contactMobile) {
+        this.contactMobile = contactMobile;
+    }
+
+    public LocalDate getShowDate() {
+        return showDate;
+    }
+
+    public void setShowDate(LocalDate showDate) {
+        this.showDate = showDate;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    public List<String> getSelectedSeats() {
+        return selectedSeats;
+    }
+
+    public void setSelectedSeats(List<String> selectedSeats) {
+        this.selectedSeats = selectedSeats;
+    }
+}
